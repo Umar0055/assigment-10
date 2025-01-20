aa1bc52a2193cf2b9f813d65632b90bf04ee867a
umar0055 <umarsohail648@gmail.com>
2025-01-20 18:15:54 +0500

    assigment no 10

---
+++ scrip.js
@@ -0,0 +1,71 @@
+
+//assigment no1
+// function clickme(){
+//     var userasns=document.getElementById('usertext').value
+    
+//     var results=document.getElementById('result')
+//     for(var i =0;i<=userasns.length;i++){
+//         results.innerHTML=`result of this string is ${i}`
+//     }
+    
+// }
+
+
+
+
+
+//assigment no 4
+// function clickme(){
+//     var userasns=document.getElementById('usertext').value
+//     var userasns2=document.getElementById('usertext2').value
+//     var results=document.getElementById('result')
+//     if(userasns === userasns2){
+//         results.innerHTML=`result:compraed`
+//     }else{
+//          results.innerHTML=`result: not compraed`
+//     }
+// }
+
+// assigment no 5
+// function clickme(){
+//     var userasns=document.getElementById('usertext').value
+
+//     var results=document.getElementById('result')
+
+//        results.innerHTML =userasns.toUpperCase()
+    
+    
+// }
+
+//assigment no 6
+// function clickme(){
+//     var userasns=document.getElementById('usertext').value
+
+//     var results=document.getElementById('result')
+
+//        results.innerHTML =userasns.toLowerCase()
+    
+    
+// }
+
+//assigment no 10
+// function clickme(){
+//     var userasns=document.getElementById('usertext').value
+    
+//     var results=document.getElementById('result')
+//     for(var i =0;i<=userasns.length;i++){
+//         results.innerHTML=`result :total num in this string is ${i}`
+//     }
+    
+// }
+
+// assigment no 11
+// function clickme(){
+//     var usernums=document.getElementById('usernum').value
+
+//     var results=document.getElementById('result')
+
+//        results.innerHTML =parseInt(usernums.toString().split('').reverse().join(''))
+    
+    
+// }
