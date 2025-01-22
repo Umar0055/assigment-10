5d49ae0addd4d00ae88afb921a75ff21c6b49a61
umar0055 <umarsohail648@gmail.com>
2025-01-22 17:55:38 +0500

    string assigemnt no 10 in dom

--- scrip.js
+++ scrip.js
@@ -1,23 +1,18 @@
 
-//assigment no1
+//assigment no 1
 // function clickme(){
 //     var userasns=document.getElementById('usertext').value
-    
 //     var results=document.getElementById('result')
 //     for(var i =0;i<=userasns.length;i++){
 //         results.innerHTML=`result of this string is ${i}`
 //     }
-    
 // }
 // assigment no 2 
 //  function clickme(){
 //         var userasns=document.getElementById('usertext').value
-    
 //         var  userasnss= userasns
-        
 //         console.log(userasns)
 //         console.log(userasnss)
-        
 //     }
 
 // assigment no 3
@@ -27,7 +22,6 @@
 //         var userstext = userasns +userasnss
 //         var results=document.getElementById('result')
 //         results.innerHTML =userstext
-        
 //     }
 
 //assigment no 4
@@ -45,64 +39,90 @@
 // assigment no 5
 // function clickme(){
 //     var userasns=document.getElementById('usertext').value
-
 //     var results=document.getElementById('result')
-
 //        results.innerHTML =userasns.toUpperCase()
-    
-    
 // }
 
 //assigment no 6
 // function clickme(){
 //     var userasns=document.getElementById('usertext').value
-
 //     var results=document.getElementById('result')
-
 //        results.innerHTML =userasns.toLowerCase()
-    
-    
+// }
+
+//assigment no 8
+// function clickme(){
+//     var userasns=document.getElementById('usertext').value
+//     var results=document.getElementById('result')
+//     for(var i =0;i<=userasns.length;i++){
+//         results.innerHTML=`result :total num in this string is ${i}`
+//     }
+// }
+
+//assigment no 9
+// function clickme() {
+//     var userstext=document.getElementById('usertext').value
+//     var results=document.getElementById('result')
+//     if (userstext === 'a' || userstext === 'e' || userstext === 'e' || userstext === 'i' || userstext === 'o' || userstext === 'u') {
+//         results.innerHTML =`${userstext} is a vowel`
+//     } else {
+//         results.innerHTML =`${userstext} is a consonants `
+//     }
 // }
 
 //assigment no 10
 // function clickme(){
 //     var userasns=document.getElementById('usertext').value
-    
 //     var results=document.getElementById('result')
 //     for(var i =0;i<=userasns.length;i++){
 //         results.innerHTML=`result :total num in this string is ${i}`
 //     }
-    
 // }
 
 // assigment no 11
 // function clickme(){
 //     var userstext=document.getElementById('usertext').value
-
 //     var results=document.getElementById('result')
-
 //        results.innerHTML =userstext.split('').reverse().join('')
-    
-    
+// }
+//assigment no 13
+// function clickme(){
+// var userstext=document.getElementById('usertext').value
+//   var results=document.getElementById('result')
+//   results.innerHTML =userstext.charAt(0)
 // }
 
+//assigment no 14
+// function clickme(){
+// var userstext=document.getElementById('usertext').value
+//   var results=document.getElementById('result')
+//   for (let i = 0; i < userstext.length; i++) {
+//     results.innerHTML =userstext.charAt(i)
+    
+//   }
+// }
+//assigment no 15
+// function clickme(){
+//     var userstext=document.getElementById('usertext').value
+//       var results=document.getElementById('result')
+//       for (let i = 0; i <= userstext.length; i++) {
+//         results.innerHTML =` total words in this string is ${i}`
+        
+//       }
+//     }
 
 // assigment no 21
 //  function clickme(){
 //         var userasns=document.getElementById('usertext').value
 //         var userword =userasns.slice(1)
-       
 //         var results=document.getElementById('result')
 //         results.innerHTML =userword
-        
 //     }
 
 // assigment no 22
 //  function clickme(){
 //         var userstext=document.getElementById('usertext').value
-        
 //        var userword =userstext.split('').reverse().join('').slice(1)
- 
 //         var results=document.getElementById('result')
 //         results.innerHTML =userword.split('').reverse().join('')
 //     }
@@ -110,9 +130,6 @@
 //assigment no 23
 // function clickme(){
 //             var userstext=document.getElementById('usertext').value
-            
-           
-     
 //             var results=document.getElementById('result')
 //             for (var i= 0; i<=userstext.length;i--){
 //             results.innerHTML =` result is ${userstext.split('').reverse().join('').slice(1)}`
@@ -123,9 +140,6 @@
 // // function clickme(){
 
 //             var userstext=document.getElementById('usertext').value
-            
-           
-     
 //             var results=document.getElementById('result')
 //             results.innerHTML =userstext.replace("i","we")
 //         }
@@ -133,9 +147,7 @@
 //assigment no 26
 // function clickme(){
 //     var userstext=document.getElementById('usertext').value
-    
-   
-
 //     var results=document.getElementById('result')
 //     results.innerHTML =userstext.replace("js","php")
 // }
+
